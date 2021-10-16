e7a33e8b8ef212bcaff149a26693b567f44925c0
Sreenath D <sreenathd2002@gmail.com>
2021-10-16 18:35:02 +0530

    Update server_data_collect.py

--- server_data_collect.py
+++ server_data_collect.py
@@ -16,12 +16,7 @@ class runServerCmd:
                          shell=False, 
                          stdout=subprocess.PIPE, 
                          stderr=subprocess.PIPE)
-        result = ' '
-        while True:
-            line = resp.stdout.readline()
-            if not line:
-                break
-            result.join(line.decode("utf-8"))
+        result, err = resp.communicate()
         return result
     
     def exec_cmd2(self, cmd):

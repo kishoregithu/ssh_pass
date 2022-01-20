b39cd53dac9ff2d37708d39cd3b6ff4500f81d7f
Sreenath D <sreenathd2002@gmail.com>
2022-01-20 14:44:00 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,30 @@
+import paramiko
+import sys
+import ssh_lib
+import unittest
+from unittest.mock import MagicMock
+from unittest.mock import patch
 
+class TestStringMethods(unittest.TestCase):
+    def test_snd_cmd(self):
+        
+        paramiko.SSHClient.set_missing_host_key_policy =  MagicMock(return_value=True)
+        paramiko.SSHClient.connect = MagicMock()
+        paramiko.SSHClient.invoke_shell = MagicMock()
+        paramiko.SSHClient.exec_command =  MagicMock(return_value=[sys.stdin,sys.stdout,sys.stderr])
+        paramiko.SSHClient.get_transport =  MagicMock()
+        sshc = ssh_lib.SSHsession()
+        sshc._read_stdout = MagicMock(return_value='1')
+        self.assertEqual(sshc.send_cmd('ls'),'1')
+    
+    def test_snd_cmd_except(self):
+        
+        paramiko.SSHClient.set_missing_host_key_policy =  MagicMock(return_value=True)
+        paramiko.SSHClient.connect = MagicMock()
+        paramiko.SSHClient.invoke_shell = MagicMock()
+        paramiko.SSHClient.exec_command =  MagicMock(return_value=[sys.stdin,sys.stdout,sys.stderr])
+        paramiko.SSHClient.exec_command.side_effect = paramiko.SSHException
+        paramiko.SSHClient.get_transport =  MagicMock()
+        sshc = ssh_lib.SSHsession()
+        sshc._read_stdout = MagicMock(return_value='1')
+        self.assertEqual(sshc.send_cmd('ls'), None)

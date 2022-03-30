4224208730f96326fcee5a0d9208cb0c4d73a2c5
Sreenath D <sreenathd2002@gmail.com>
2022-03-30 10:25:41 +0530

    Update scp_a_file.py

--- scp_a_file.py
+++ scp_a_file.py
@@ -6,4 +6,10 @@ PATH = ''
 FILE = './local_file.log'
 RURL = "{}@{}:{}".format(USER,SERVER,PATH)
 
-subprocess.run(["scp", RURL , FILE ])
+#subprocess.run(["scp", RURL , FILE ])
+
+resp = subprocess.Popen(["scp",RURL, FILE],
+                         shell=False,
+                         stdout=subprocess.PIPE,
+                         stderr=subprocess.PIPE)
+result, err = resp.communicate()

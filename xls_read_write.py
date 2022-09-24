dd1b4a742108568a67b4ada8f3845db564dd0234
Sreenath D <sreenathd2002@gmail.com>
2022-09-24 08:27:49 +0530

    Update xls_read_write.py

--- xls_read_write.py
+++ xls_read_write.py
@@ -5,15 +5,27 @@ book = xlwt.Workbook()
 sheet1 = book.add_sheet('CURRENT')
 sh = wb.sheet_by_index(0)
 
+group_list = {
+    "colnames": ['group_name', 'Bankers Trust', 'Centex', 'MISC G2 Subprime', 'SBMSI Master'],
+    'abs':['_BT', '_CX', '_M2', '_SB'], 
+    'mbs':[ 'BTR', '', '', 'SBA'],
+    'alt_a':[ 'ZBT', '', 'ZM2', 'ZSB']
+}
 col_values = sh.col_values(1)
 #col_values = col_values[1:len(col_values)] 
 for j,k in enumerate(col_values):
     row_values = sh.row_values(j)
-    #row_values =  row_values[1:len(row_values)]
-    #print(k, row_values[7:10])
-    if 'ABS' in row_values[8]:
-        style = xlwt.easyxf('pattern: pattern solid, fore_colour green;')
-        print(j, row_values[8])
-        sheet1.write(j, 7, 'ABS', style)
-
+    if k in group_list['colnames']:
+        for row_name in ['abs','mbs', 'alt_a']:
+            for row_value in group_list[row_name]:
+                for m,i in enumerate(row_values):
+                    if i == row_value:
+                        style = xlwt.easyxf('pattern: pattern solid, fore_colour green;')
+                        print(j, i, m)
+                        sheet1.write(j, m , i , style)
+                    else:
+                        sheet1.write(j, m , i )
+     else:
+        for m,i in enumerate(row_values): 
+            sheet1.write(j, m , i )
 book.save('test.xls')

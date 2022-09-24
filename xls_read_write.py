dc8d712f5d4c3425bd4a0261b2646f58dc3c28ed
Sreenath D <sreenathd2002@gmail.com>
2022-09-24 08:10:17 +0530

    Update xls_read_write.py

--- xls_read_write.py
+++ xls_read_write.py
@@ -4,16 +4,16 @@ wb = xlrd.open_workbook('MONTHLY SHIPMENT LOG TEMPLATE.xls')
 book = xlwt.Workbook()
 sheet1 = book.add_sheet('Sheet 1')
 sh = wb.sheet_by_index(0)
-for i in range(0,2): 
-    col_values = sh.col_values(i)
-    #col_values = col_values[1:len(col_values)] 
-    for j,k in enumerate(col_values):
-        row_values = sh.row_values(j)
-        #row_values =  row_values[1:len(row_values)]
-        #print(k, row_values[7:10])
-        if 'ABS' in row_values[8]:
-            style = xlwt.easyxf('pattern: pattern solid, fore_colour green;')
-            print(j, row_values[8])
-            sheet1.write(j, 7, 'ABS', style)
+
+col_values = sh.col_values(1)
+#col_values = col_values[1:len(col_values)] 
+for j,k in enumerate(col_values):
+    row_values = sh.row_values(j)
+    #row_values =  row_values[1:len(row_values)]
+    #print(k, row_values[7:10])
+    if 'ABS' in row_values[8]:
+        style = xlwt.easyxf('pattern: pattern solid, fore_colour green;')
+        print(j, row_values[8])
+        sheet1.write(j, 7, 'ABS', style)
 
 book.save('test.xls')

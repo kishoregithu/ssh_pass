5edf10929353cc966a3f73f7e53b4b6daa3921b3
Sreenath D <sreenathd2002@gmail.com>
2022-11-15 20:15:07 +0530

    Update tsr_api.py

--- isg/tsr_api.py
+++ isg/tsr_api.py
@@ -1,10 +1,10 @@
 #tsr_api.py
-def post_export_tsr_report(self, user, domain, format, file_name, show_detail_by_did, show_transfer_duration_info):
+def post_export_tsr_report(self, user=CURRENT_USER, domain=NUANCE_DOMAIN, file_name, file_format, show_detail_by_did, show_transfer_duration_info):
     """
     API - Export the Telephony Summary Report based on user’s selection in various file formats
     :param user: logged in user
     :param domain: current domain 
-    :param format: file format (csv, xml, pdf)
+    :param file_format: file format (csv, xml, pdf)
     :param file_name: full path of the file
     :param show_detail_by_did: Show Detail by DID
     :param show_transfer_duration_info: Show transfer duration information
@@ -19,84 +19,27 @@ def post_export_tsr_report(self, user, domain, format, file_name, show_detail_by
     response = self.util.send_request(const.GET, url, params=params, headers=headers)
     return response
 
-
+tsr_helper
+def build_export_tsr_usage_report_payload(org_id=BVPAYS_ORGID, app_ids=NUANCE_APP_ID, called_parties=CALLED_PARTY_LIST,
+                                          call_type=INBOUND_CALL_TYPE, wrc_billable=WCR_BILL_FALSE, start_date=TEST_START_DATE, end_date=TEST_END_DATE, time_zone=TIME_ZONE_PACEFIC):
+    
+    return data
+    
 #test_user_controller.py
-@pytest.mark.parametrize("domain, user, role",[(NUANCE_DOMAIN, DAC_OPERATOR, ROLE_OPERATOR),
-                                               (NUANCE_DOMAIN, DAC_ADMIN, ROLE_ADMIN) ])
-def test_get_user_priviliages_for_app(self,domain, user):
+@pytest.mark.parametrize("file_format",[(CSV_FORMAT), (XML_FORMAT), (PDF_FORMAT) ])
+def test_export_valid_tsr_report_file(self,file_format):
     """
-    This test verified the priviliages of an user for an app
+    This Test Varifies:
+    1. Export TSR by giving all valid details and format as CSV.
+    2. Export TSR by giving all valid details and format as XML.
+    3. Export TSR by giving all valid details and format as PDF.
     """
-    response = self.get_tsr_user_priv_for_app(domain, user, DAC)
-    response_json = json.loads(response.text)
+    data = self.build_export_tsr_usage_report_payload()
+    file = RANDOM_FILE_NAME
+    response = self.post_export_tsr_report(data, file, file_format)
     assert HTTP_OK ==  response.status
-    assert role == response_json [0]["applicationRoles"][0]
-    assert "DAC" ==  response_json [0]['applicationName']
     
     
-def test_get_user_priviliages_for_app_role(self,domain, user):
-    """
-    This test verified the priviliages of an user for an app role
-    """
-    response = self.get_tsr_user_priv_for_app(NUANCE_DOMAIN, DAC_OPERATOR, NCP)
-    response_json = json.loads(response.text)
-    assert HTTP_OK ==  response.status
-    assert "NCP" ==  response_json [0]['applicationName']
-    assert "ROLE_NCP_FRONTEND" ==  response_json [0]['applicationRole']
     
-@pytest.mark.parametrize("domain, user, role, app ",[(NUANCE_DOMAIN, COM_OPERATOR, ROLE_OPERATOR, COM),
-                                               (NUANCE_DOMAIN, COM_ADMIN, ROLE_ADMIN, COM),
-                                               (NUANCE_DOMAIN, COM_VIEWER, ROLE_VIEWER, COM),
-                                               (NUANCE_DOMAIN, COM_ADMIN, ROLE_ADMIN, PUR),
-                                               (NUANCE_DOMAIN, COM_OPERATOR, ROLE_VIEWER, PUR),
-                                              ])
-def test_get_user_priviliages_for_app_access(self,domain, user):
-    """
-    This test verified the priviliages of an user for an app with access
-    """
-    response = self.get_tsr_user_priv_for_app(domain, user, app)
-    response_json = json.loads(response.text)
-    assert HTTP_OK ==  response.status
-    assert app ==  response_json [0]['applicationName']
-    assert role == assert "Administrator" == response_json [0]["applicationRoles"][0]
-    
-@pytest.mark.parametrize("domain, user, app ",[(NUANCE_DOMAIN, APP_OPERATOR, AM),
-                                               (NUANCE_DOMAIN, APP_ADMIN, AM),
-                                              ])
-def test_get_user_priviliages_for_app_access_negative(self,domain, user):
-    """
-    This test verified the priviliages of an user for an app with no access
-    """
-    response = self.get_tsr_user_priv_for_app(domain, user, app)
-    response_json = json.loads(response.text)
-    assert HTTP_FORBIDDEN ==  response.status
-    assert app ==  response_json [0]['applicationName']
-    
-@pytest.mark.parametrize("domain, user, role, app ",[(NUANCE_DOMAIN, LOG_OPERATOR, ROLE_OPERATOR, LV),
-                                               (NUANCE_DOMAIN, LOG_ADMIN, ROLE_ADMIN, LV),
-                                               (NUANCE_DOMAIN, TSR_OPERATOR, ROLE_OPERATOR, TSR),
-                                               (NUANCE_DOMAIN, TSR_ADMIN, ROLE_ADMIN, TSR)
-                                              ])
-def test_get_user_priviliages_for_app_no_access(self,domain, user):
-    """
-    This test verified the priviliages of an user for an app with no access
-    """
-    response = self.get_tsr_user_priv_for_app(domain, user, app)
-    response_json = json.loads(response.text)
-    assert HTTP_OK ==  response.status
-    assert app ==  response_json [0]['applicationName']
-    assert role == assert "Administrator" == response_json [0]["applicationRoles"][0]
-    
-def test_get_user_priviliages_for_app_invalid_domain(self):
-    """
-    This test verified the priviliages of an user for an app with invalid domain
-    """
-    response = self.get_tsr_user_priv_for_app(const.NULL, DAC_ADMIN, DAC)
-    assert HTTP_FORBIDDEN ==  response.status
-    response = self.get_tsr_user_priv_for_app('', DAC_ADMIN, DAC)
-    assert HTTP_FORBIDDEN ==  response.status
-    response = self.get_tsr_user_priv_for_app('abcd', DAC_ADMIN, DAC)
-    assert HTTP_FORBIDDEN ==  response.status
-    response = self.get_tsr_user_priv_for_app(None, DAC_ADMIN, DAC)
-    assert HTTP_FORBIDDEN ==  response.status
+
     

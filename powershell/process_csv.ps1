5d87903dd475736a4cb7c6733eb5f69e01859531
Sreenath D <sreenathd2002@gmail.com>
2022-11-18 05:00:05 +0530

    Update process_csv.ps1

--- powershell/process_csv.ps1
+++ powershell/process_csv.ps1
@@ -1,8 +1,7 @@
 $filePath = 
-Import-Csv $filePath | Select-Object $desiredColumnsSystem | Sort RESOURCEID -Unique | ForEach-Object {
-            If ($_.USER_NAME -match $Regex) {
-                $_.USER_NAME = $_.USER_NAME -replace $Regex,''
-            }
-            $_
-        } |
-        Export-Csv -Path $System –NoTypeInformation
+Import-Csv $filePath | ForEach-Object {
+    if ($_.Enabled -eq 'True') {
+        $_.Enabled = 'A'
+    }
+    $_
+} | Export-Csv .\test-modified.csv -NoTypeInformation
